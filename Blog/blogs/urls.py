ae35a4d8a864004bd3ac50a5c7064d8270798466
luiggy macias <ferrinluiggy@gmail.com>
2022-03-18 14:18:40 -0500

    implementing user owning data

--- Blog/blogs/urls.py
+++ Blog/blogs/urls.py
@@ -5,6 +5,7 @@ app_name = 'blogs'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('blogs/', views.blogs, name='blogs'),
     path('new_post', views.new_post, name='new_post'),
     path('edit_post/<int:post_id>', views.edit_post, name='edit_post'),
 ]
--- Blog/blogs/views.py
+++ Blog/blogs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from blogs.forms import BlogPostForm
 from .models import BlogPost
@@ -12,6 +13,16 @@ def index(request):
     }
     return render(request, 'blogs/index.html', context)
 
+@login_required
+def blogs(request):
+    """The home page for Blogs."""
+    # print(request.user)
+    blog_posts = BlogPost.objects.filter(owner=request.user).order_by('-date_added')
+    context = {
+        'blog_posts': blog_posts,
+    }
+    return render(request, 'blogs/blogs.html', context)
+@login_required
 def new_post(request):
     """Add a new blog post."""
     if request.method != 'POST':
@@ -21,12 +32,15 @@ def new_post(request):
         # POST data submitted; process data.
         form = BlogPostForm(data=request.POST)
         if form.is_valid():
-            new_post = form .save()
-            return redirect('blogs:index')
+            new_post = form .save(commit=False)
+            new_post.owner = request.user
+            new_post.save()
+            return redirect('blogs:blogs')
      # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'blogs/new_post.html', context)
 
+@login_required
 def edit_post(request, post_id):
     """Edit a blog post."""
     post = BlogPost.objects.get(id=post_id)
@@ -39,6 +53,6 @@ def edit_post(request, post_id):
         form = BlogPostForm(instance=post, data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect('blogs:index')
+            return redirect('blogs:blogs')
     context = {'post': post, 'form': form}
     return render(request, 'blogs/edit_post.html', context)
